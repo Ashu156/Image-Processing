40dd47019b438f4bb4da3ab5c8cb998553774295
Ashu156 <35688705+Ashu156@users.noreply.github.com>
2021-07-04 19:22:16 +0530

    Update CropVideo.m

--- CropVideo.m
+++ CropVideo.m
@@ -7,7 +7,7 @@ open(writerObj1);
 for i = 100: 200
   im = read(vid1,i);
   im=imresize(im,0.5);
-  imc=imcrop(im,[60 60 300 300]);% The dimention of the new video
+  imc=imcrop(im,[60 60 300 300]);% The dimension of the new video
   img=rgb2gray(im);
   [a,b]=size(img);
   imc=imresize(imc,[a,b]);
